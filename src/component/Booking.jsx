72a091d3745b1a4a4dd0f22136c47000b782a97d
AMANPATEL <amanpatel8844382@gmail.com>
2024-07-20 15:18:17 +0530

    Design Completed Hotel Management System

--- src/component/Booking.jsx
+++ src/component/Booking.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Booking = () => {
-  return <div>Find My Booking Page</div>;
+  return (
+    <div>
+      {" "}
+      <div className="findbooking">Find My Booking</div>
+      <div className="findbookinginputbtn">
+        <input
+          className="findbookinginput"
+          placeholder="Enter your booking confirmation code"
+          type="text"
+          name=""
+          id=""
+        />
+        <button className="findroombtn">Find</button>
+      </div>
+    </div>
+  );
 };
 
 export default Booking;
--- src/component/Room.jsx
+++ src/component/Room.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Rooms = () => {
-  return <div>Rooms Page</div>;
+  return (
+    <div className="roompage">
+      <div className="allroomtitle">All Rooms</div>
+      <div className="roompagedata">
+        <div className="filterroomtype">
+          <div className="filtertitile">Filter by Room Type:</div>
+          <div className="filterall">All</div>
+        </div>
+
+        <div className="data">
+          <div className="outer1">
+            Check-In-date
+            <div className="inner1">Select Check In Date</div>
+          </div>
+          <div className="outer2">
+            Check-In-date
+            <div className="inner2">Select Check In Date</div>
+          </div>
+          <div className="outer3">
+            Check-In-date
+            <div className="inner3">Select Check In Date</div>
+          </div>
+          <a className="homebtnsearchroom">Search Rooms</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rooms;
